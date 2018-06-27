b7b468e7913c97d31285346fbbe9b92f9783ea49
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-06-27 20:38:31 +0800

    Add project files.

---
+++ ASPNetCoreAppHostOnWs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+
+namespace ASPNetCoreAppHostOnWs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(pathToContentRoot)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:4312")
+                .Build();
+
+            host.RunAsService();
+        }
+    }
+}
